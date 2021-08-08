82cf5c8c00ebd8e20442524a9635be0179410a2f
HARIPRASAD S A <myharicode@gmail.com>
2021-08-08 13:44:18 +0530

    conditional rendering in JSX

--- src/App.js
+++ src/App.js
@@ -1,11 +1,53 @@
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+const app = {
+  title: "Indecision app",
+  subtitle:"Put your life in the hands of a computer",
+  options:['one','two']
+}
 
+
+// eslint-disable-next-line 
+function App() {
+  
+  return (
+    <div className="App alert alert-success">
+      <h1>{app.title}</h1>
+      { (app.subtitle) && <p>{app.subtitle}</p>}
+      <p>{ (app.options.length>0) ? "Here are your options" : "No options avilables"}</p>
+      <ol>
+      {app.options.map(itm => <li>{itm}</li>)}
+      </ol>
+      
+      
     </div>
   );
 }
 
+const user = {
+   username : "hariprasad",
+   userAge : 26,
+   userLocation : "chennai"
+}
+
+
+function getLocation(location){
+  if(location) return <p>{location}</p>
+  return "Unknown"
+}
+
+//Undefined null and boolean are ignored by jsx
+// eslint-disable-next-line 
+function App1() {
+  return(
+    <div>
+      <h1>{user.username ? user.username : "Anonymous"}</h1>
+      {(user.age && user.age>=18) && <p>Age:{user.userAge}</p>}
+      {getLocation(user.userLocation)}
+    </div>
+    
+  )
+}
+
 export default App;
+
